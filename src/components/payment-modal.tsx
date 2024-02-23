61e613818f6d93dce1f0619d6f71770e1477d8b0
Nicolás Agüero <nico.agueroo99@gmail.com>
2024-02-23 00:13:39 -0300

    Update payment-modal.tsx

--- src/components/payment-modal.tsx
+++ src/components/payment-modal.tsx
@@ -1,29 +1,29 @@
-'use client'
+// 'use client'
 
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+// import { useRouter, useSearchParams } from 'next/navigation'
+// import { useEffect, useState } from 'react'
 
 export default function PaymentModal (): JSX.Element | null {
-  const router = useRouter()
-  const search = useSearchParams()
-  const [isApproved, setIsApproved] = useState<string | null>(null)
+  // const router = useRouter()
+  // const search = useSearchParams()
+  // const [isApproved, setIsApproved] = useState<string | null>(null)
 
-  useEffect(() => {
-    const checkApproved = search.get('success') !== null ? 'success' : search.get('failure') !== null ? 'failure' : null
-    setIsApproved(checkApproved)
-  }, [search])
+  // useEffect(() => {
+  //   const checkApproved = search.get('success') !== null ? 'success' : search.get('failure') !== null ? 'failure' : null
+  //   setIsApproved(checkApproved)
+  // }, [search])
 
-  if (isApproved === null) {
-    return null
-  }
+  // if (isApproved === null) {
+  //   return null
+  // }
 
-  const handleAccept = (): void => {
-    router.replace('/')
-  }
+  // const handleAccept = (): void => {
+  //   router.replace('/')
+  // }
 
   return (
     <>
-      <div className="w-screen h-screen fixed top-0 left-0 bg-black/30"></div>
+{/*       <div className="w-screen h-screen fixed top-0 left-0 bg-black/30"></div>
       <div className="w-3/5 h-[250px] fixed top-0 left-0 right-0 bottom-0 m-auto px-4 py-4 rounded-lg shadow-lg bg-white">
         {
           isApproved === 'success'
@@ -37,7 +37,7 @@ export default function PaymentModal (): JSX.Element | null {
               </>
         }
         <button onClick={handleAccept} className="bg-slate-500 text-white px-4 py-2 rounded-lg">Aceptar</button>
-      </div>
+      </div> */}
     </>
   )
 }
